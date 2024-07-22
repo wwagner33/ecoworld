7ff04c6742f17472323dcf704e62feefa5127bd7
Andravi <andravi75@gmail.com>
2024-07-22 12:01:27 -0300

    painel added

--- ambiente-de-simulacao/isoworld/Simulator.py
+++ ambiente-de-simulacao/isoworld/Simulator.py
@@ -1,9 +1,11 @@
 import sys
 import json
 import pygame
+import pygame_gui
 from pygame.locals import *
 from ImageManager import ImageManager
 from PlayerAgent import PlayerAgent
+from SimulatorThings.Gui import Gui
 from World import World
 from Agent import Agent
 from Config import Config
@@ -17,7 +19,7 @@ class Simulator:
         pygame.display.set_caption('Mundo dos Agentes')
         self.images = ImageManager()
         self.clock = pygame.time.Clock()
-
+        self.gui = Gui(self.screen)
         self.player = PlayerAgent(x=0, y=0, image=self.images.object_images[2])
         self.world = self._load_world(simulation_map, self.player)
         self.agents = [self.player]
@@ -100,11 +102,14 @@ class Simulator:
                         self.player.move(-1,0)
                     elif event.key == pygame.K_d:
                         self.player.move(1,0)
+                self.gui.manager.process_events(event)
+
 
             self.update_game_state()
             self.render_game()
+            delta_time =self.clock.tick(Config.MAX_FPS) / 1000.0
+            self.gui.render(delta_time)
             pygame.display.flip()
-            self.clock.tick(Config.MAX_FPS)
 
 
     def update_game_state(self):
--- ambiente-de-simulacao/isoworld/SimulatorThings/Gui.py
+++ ambiente-de-simulacao/isoworld/SimulatorThings/Gui.py
@@ -1,3 +1,6 @@
+import pygame
+import pygame_gui
+from pygame_gui.elements.ui_panel import UIPanel 
 
 
 class Gui(object):
@@ -7,4 +10,15 @@ class Gui(object):
     
     def __init__(self, screen) -> None:
         self.screen = screen
-        self.manager
+        self.manager = pygame_gui.UIManager((970, 640), theme_path="themes.json")
+        self.painel = UIPanel(relative_rect=pygame.Rect((0, 0),(200, 640)))
+        self.hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((10, 275), (100, 50)),
+                                             text='Say Hello',
+                                             manager=self.manager)
+    
+    def render(self, delta_time):
+        self.manager.update(delta_time)
+        self.manager.draw_ui(self.screen)
+        self.manager.set_visual_debug_mode(True)
+
+
